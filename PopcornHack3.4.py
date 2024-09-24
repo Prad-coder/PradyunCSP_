c450a1fea6fd9770ad90b028c3e151c124ee29c8
Prad-coder <pradyungowda@gmail.com>
2024-09-24 09:23:47 -0700

    modified:   PopcornHack3.4.py commit

--- PopcornHack3.4.py
+++ PopcornHack3.4.py
@@ -1,8 +1,8 @@
 string = "Brawl Stars is a fun game!"
 string2 = "Tacocat"
-print("String 1:")
+print("String 1: Brawl Stars is a fun game!")
 length = len(string)
-print(length)
+print("Length:", length)
 def count_vowels(input_string):
     vowels = 'aeiouAEIOU'
     count = 0
@@ -10,7 +10,7 @@ def count_vowels(input_string):
         if char in vowels:
             count += 1
     return count
-print(count_vowels(string))
+print("Vowel Count:", count_vowels(string))
 def average_word_length(input_string):
     words = input_string.split()
     if not words:
@@ -18,14 +18,14 @@ def average_word_length(input_string):
     total_length = sum(len(word) for word in words)
     average_length = total_length / len(words)
     return average_length
-print(average_word_length(string))
+print("Average Word Count:", average_word_length(string))
 def palindrome(input_string):
     string = input_string.replace(" ","").lower()
     return string == string[::-1]
-print(palindrome(string))
-print("String 2:")
+print("Palindrome or Not?", palindrome(string))
+print("String 2: Tacocat")
 length2 = len(string2)
-print(length2)
-print(count_vowels(string2))
-print(average_word_length(string2))
-print(palindrome(string2))
+print("Length:", length2)
+print("Vowel Count:", count_vowels(string2))
+print("Average Word Count:", average_word_length(string2))
+print("Palindrome or Not?", palindrome(string2))
